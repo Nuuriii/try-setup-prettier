fce9210db60b09f06fe5ef6a69ab94357e7fcf3f
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-09-08 23:52:51 +0700

    feat: add new text for testing

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          udjjdjdjddjj&nbsp;
+          uddjj&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
         <div>
